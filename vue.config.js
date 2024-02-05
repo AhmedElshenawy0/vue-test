befe938efe993df68c9375dc967ff6ed1fe89c82
Ahmed Elshenawy <110618964+AhmedElshenawy0@users.noreply.github.com>
2024-02-05 08:53:16 +0200

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: "/vue-task/",
+  publicPath: "/vue-task2/",
 });
